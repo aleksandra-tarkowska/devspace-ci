d22e6c6a47ec304fbf05b595d7fefa8f28ab1204
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2017-04-21 14:09:30 +0100

    store entire home dir in volume

--- git/run.sh
+++ git/run.sh
@@ -2,16 +2,12 @@
 
 ssh-keyscan github.com >> /home/omero/.ssh/known_hosts
 
-chown omero:omero -R /home/omero/.ssh
-chmod 700 -R /home/omero/.ssh
+chown omero:omero -R /home/omero
+chown omero:omero -R /src
 
-chown omero:omero /home/omero/.gitconfig
+chmod 700 -R /home/omero/.ssh
 chmod 700 /home/omero/.gitconfig
 
-chown omero:omero -R /home/omero/.m2
 
-chown omero:omero -R /home/omero/workspace
-
-chown omero:omero -R /src
 
 /usr/sbin/sshd -eD
